bb805e8911cbee2cebdf810e9722f19d60c697b5
Owens <fotsingsadeu@gmail.com>
2026-06-10 22:13:41 +0100

    add API

---
+++ app/api/catalogue/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+let catalogues = [];
+
+export async function GET(){
+    return NextResponse.json(catalogues);
+}
+
+export async function POST(request:Request){
+    const body = await request.json();
+    const newCatalogue ={
+        id: Date.now(),
+        title: body.title,
+        completed: false,
+    };
+    catalogues.push(newCatalogue);
+    return NextResponse.json(newCatalogue);
+}
+
+export async function PUT(request:Request){
+    const body = await request.json();
+    catalogues = catalogues.map((catalogue) =>
+        catalogue.id === body.id ? { ...catalogue, completed: !catalogue.completed }: catalogue,
+);
+return NextResponse.json({ message:" Catalogue mise a jour"});
+}
+
+ export async function DELETE(request:Request) {
+    const body = await request.json();
+    catalogues = catalogues.filter((catalogue) => catalogue.id !== body.id);
+    return NextResponse.json ({ message: 'Catalogue supprime'});
+} 
